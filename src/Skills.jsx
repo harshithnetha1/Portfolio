71a56cbec30e2f510dd32e606a38aa2c2a86796b
harshithnetha1 <harshithnetha1@gmail.com>
2024-07-13 23:56:17 +0530

    experience

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -31,9 +31,7 @@ export default function Skills() {
       <div  className='developer-text'>
       <h3  style={{color:'white',position:'relative', fontSize: '50px'}}><span className="italic-text" >my</span> Experience</h3>
       <span style={{color:'white',padding:'20px', fontSize:'30px'}}>
-         I have been a front end developer for the past two years, working on various projects.
-         I am currently adding MERN full stack skills to my toolkit and have completed several personal projects.
-         Additionally, I have Python skills and aspire to pursue a career in data science.
+      With a robust two-year background in front-end development, I have successfully contributed to various projects, showcasing my expertise in creating dynamic and user-friendly web applications. My journey as a developer has been marked by a dedication to continuous learning and skill enhancement.
       </span>
       <div className='developer-text'>
       <h3  style={{color:'white',position:'relative', fontSize: '50px'}}><span className="italic-text" >my</span> Education</h3>
