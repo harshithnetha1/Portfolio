985f3fdae36d4f3f283f3dbe0392715cbc59c04a
harshithnetha1 <harshithnetha1@gmail.com>
2026-02-26 06:02:59 +0530

    all

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -15,7 +15,7 @@ export default function Skills() {
         <span className="italic-text" style={{color:'white',position:'relative', fontSize: '50px'}}>the Resume</span>
         <br className="mobile-break" />
         <span style={{color:'white',padding:'20px', fontSize:'30px'}}>
-    I have been a front end developer for the past two years, working on various projects.
+    I have been a front end developer for the past few years, working on various projects.
      I am currently adding MERN full stack skills to my toolkit and have completed several personal projects.
       Additionally, I have Python skills and aspire to pursue a career in data science
       </span>
@@ -31,7 +31,7 @@ export default function Skills() {
       <div  className='developer-text'>
       <h3  style={{color:'white',position:'relative', fontSize: '50px'}}><span className="italic-text" >my</span> Experience</h3>
       <span style={{color:'white',padding:'20px', fontSize:'30px'}}>
-      With a robust two-year background in front-end development, I have successfully contributed to various projects, showcasing my expertise in creating dynamic and user-friendly web applications. My journey as a developer has been marked by a dedication to continuous learning and skill enhancement.
+      With a robust 4+ years in front-end development, I have successfully contributed to various projects, showcasing my expertise in creating dynamic and user-friendly web applications. My journey as a developer has been marked by a dedication to continuous learning and skill enhancement.
       </span>
       <div className='developer-text'>
       <h3  style={{color:'white',position:'relative', fontSize: '50px'}}><span className="italic-text" >my</span> Education</h3>
